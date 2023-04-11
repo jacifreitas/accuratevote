725c1deef5be7c8e5960e33a49cca2f473ef7ef5
jacifreitas <jacifreitas21@gmail.com>
2023-04-11 16:57:22 -0300

    first commit

---
+++ src/main/java/com/jacifreitas/accuratevote/AccuratevoteApplication.java
@@ -0,0 +1,13 @@
+package com.jacifreitas.accuratevote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccuratevoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccuratevoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacifreitas/accuratevote/AccuratevoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacifreitas.accuratevote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccuratevoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
